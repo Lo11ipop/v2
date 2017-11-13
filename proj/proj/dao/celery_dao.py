64802e1db488c61070c6a62bbe8cd909137254e3
Lo11ipop <andrey.kuzmenko.94@gmail.com>
2017-11-13 22:17:39 +0200

    v2.10.2

--- proj/proj/dao/celery_dao.py
+++ proj/proj/dao/celery_dao.py
@@ -2,12 +2,12 @@ def get_last_exports(id, cur):
     cur.execute("SELECT type, from_date, to_date FROM exports WHERE id = {0};".format(id))
     return cur.fetchall()
 
-def get_lists_row_without_price(param1, param2, cur):
-    cur.execute("SELECT Name, created, updated  FROM lists WHERE '{0}' <= created AND '{1}' >= created;".format(param1, param2))
+def get_lists_row_without_price(date_from, date_to, cur):
+    cur.execute("SELECT Name, created, updated  FROM lists WHERE '{0}' <= created AND '{1}' >= created;".format(date_from, date_to))
     return cur.fetchall()
 
-def get_lists_row_with_price(param1, param2, cur):
-    cur.execute("SELECT Name, price, created, updated  FROM lists WHERE '{0}' <= created AND '{1}' >= created;".format(param1, param2))
+def get_lists_row_with_price(date_from, date_to, cur):
+    cur.execute("SELECT Name, price, created, updated  FROM lists WHERE '{0}' <= created AND '{1}' >= created;".format(date_from, date_to))
     return cur.fetchall()
 
 def update_exports(fname, id, cur):
